763b5e0885e46532cc071193c42ee247745b88f9
fenix302 <fenix302@naver.com>
2023-02-09 14:40:37 +0900

    제너럴보드 컨트롤러 때문에 에러나서 주석처리했습니다.

--- src/main/java/work/generalboard/GeneralBoardController.java
+++ src/main/java/work/generalboard/GeneralBoardController.java
@@ -1,20 +1,20 @@
-package work.generalboard;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j;
-
-@Controller
-@Log4j
-@RequestMapping("/general/*")
-@AllArgsConstructor
-public class GeneralBoardController {
-	private GeneralBoardService service;
-	
-	public void list(Model model) {
-		model.addAttribute("list", service.getList());
-	}
-}
+//package work.generalboard;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.extern.log4j.Log4j;
+//
+//@Controller
+//@Log4j
+////@RequestMapping("/general/*")
+////@AllArgsConstructor
+////public class GeneralBoardController {
+////	private GeneralBoardService service;
+////	
+////	public void list(Model model) {
+////		model.addAttribute("list", service.getList());
+////	}
+//}
